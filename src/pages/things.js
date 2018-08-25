7e768dda4b05971ebd862f607dc166adc7050d94
Everspace <7560635+Everspace@users.noreply.github.com>
2018-08-25 15:25:11 -0700

    Slap className on rendered Ast

--- src/pages/things.js
+++ src/pages/things.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Ast from "lib/Ast"
+import { css } from "emotion"
 
 export const query = graphql`
   {
@@ -21,13 +22,26 @@ export const query = graphql`
   }
 `
 
+let pageContent = css`
+  /*
+    When a react component ends up in a markdown section that would be like a paragraph,
+    it replaces the p with a div, and ruins everything.
+  */
+  & > p {
+    display: block; /* Render like div */
+  }
+  & > div {
+    padding-bottom: 1.45rem; /* Render like p for now */
+  }
+`
+
 export default ({ data }) => {
   let result = data.allExaltedArtifact.edges
     .map(({ node }) => node) // Pull out node from edge
     .map(node => (
       <div key={node.name}>
         <h2>{node.name}</h2>
-        <Ast ast={node.parent.htmlAst} node={node} />
+        <Ast className={pageContent} ast={node.parent.htmlAst} node={node} />
       </div>
     ))
 
