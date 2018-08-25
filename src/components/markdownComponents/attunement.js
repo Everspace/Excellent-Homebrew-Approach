101fa4ee402c6cba64d512cc84c23b7db2461262
Everspace <7560635+Everspace@users.noreply.github.com>
2018-08-24 22:34:23 -0700

    Reimplement attunment component

--- src/components/markdownComponents/attunement.js
+++ src/components/markdownComponents/attunement.js
@@ -12,21 +12,70 @@ let hearthstoneStyle = css`
   }
 `
 
-export const attunement = ({ cost, slots }) => {
-  let toDisplay = []
-  toDisplay.push(
-    <div className={attuneStyle} key="attunement">
-      <strong>Attunement: </strong> {cost || "none"}
-    </div>
-  )
+let spacingUnit = 1
+let card = css`
+  margin:  ${spacingUnit/2}em;
+  padding: ${spacingUnit/2}em;
+  box-shadow: 0px 4px 15px rgba(10, 10, 10, 0.15),
+              0px 4px  5px rgba(10, 10, 10, 0.25);
+`
 
-  if (slots) {
-    toDisplay.push(
-      <div className={hearthstoneStyle} key="hearthstones">
-        <strong>Hearthstone Slots:</strong> {slots}
-      </div>
-    )
+const Armor = ({weight, soak, hardness, mobilityPenalty, ...props}) => <div {...props}>
+  <span key='type'>
+    <strong>Type: </strong> {weight} Armor
+  </span>
+  <span key='stats'>
+    (+{soak} Soak, Hardness {hardness}, Mobility Penalty {mobilityPenalty})
+  </span>
+</div>
+
+const Hearthstones = ({hearthstoneslots, ...props}) => hearthstoneslots ?
+  <div className={hearthstoneStyle} {...props}>
+    <span>
+      <strong>Hearthstone Slots:</strong> {hearthstoneslots}
+    </span>
+  </div>
+  : null
+
+const Weapon = (props) => <div {...props}>Weapon Not done yet!</div>
+const Other = (props) => null
+
+let mapping = {
+  Armor,
+  Weapon,
+}
+
+export const attunement = ({tags, era, hearthstoneslots, attunement = "3m", category = "Armor", ...props }) => {
+
+  let Statline = mapping[category]
+  let stats = {}
+  switch (category) {
+    case /[Aa]rmou?r/:
+      let {weight} = props
+      stats = {
+        weight
+      }
+      break;
+    default:
+      Statline = Other
+      break;
   }
 
-  return <div>{toDisplay}</div>
+  return <div {...props} className={card}>
+    <div key="attunement">
+      <span><strong>Attunement: </strong> {attunement}</span>
+    </div>
+    <Statline key="statline" {...stats} />
+    <Hearthstones hearthstoneslots={hearthstoneslots} />
+    <div key="tags">
+      <span><strong>Tags: </strong>{
+         tags ? tags.join(', ') : "None"
+      }</span>
+    </div>
+    <div key="era">
+      <strong>Era: </strong>
+      <span>{era || "Age of Sorrows"}</span>
+    </div>
+  </div>
 }
+
