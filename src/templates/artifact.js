cd83b370c48c91132857c174af7c53621d0a3387
Aaron Haaf <ahaaf@convertus.com>
2018-08-28 14:08:25 -0700

    Make artifact template more like the things page

--- src/templates/artifact.js
+++ src/templates/artifact.js
@@ -1,15 +1,30 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "components/layout"
 import Ast from "lib/Ast"
+import { css } from "emotion";
+
+let pageContent = css`
+  /*
+    When a react component ends up in a markdown section that would be like a paragraph,
+    it replaces the p with a div, and ruins everything.
+  */
+  & > p {
+    display: block; /* Render like div */
+  }
+  & > div {
+    padding-bottom: 1.45rem; /* Render like p for now */
+  }
+`
 
 export default ({ data }) => {
   const { exaltedArtifact } = data
 
   return (
     <Layout>
+      <Link to="/artifacts">back</Link>
       <h1>{exaltedArtifact.name}</h1>
-      <Ast ast={exaltedArtifact.parent.htmlAst} node={exaltedArtifact} />
+      <Ast className={pageContent} ast={exaltedArtifact.parent.htmlAst} node={exaltedArtifact} />
     </Layout>
   )
 }
@@ -18,8 +33,9 @@ export const query = graphql`
   query exaltedArtifactQuery($name: String!) {
     exaltedArtifact(name: { eq: $name }) {
       name
-      hearthstoneSlots
+      attunement
       equipmentTags
+      hearthstoneSlots
       parent {
         ... on MarkdownRemark {
           htmlAst
