741aceb58cd7a5c22b3b15372d848ff26e61e138
Aaron Haaf <ahaaf@convertus.com>
2018-08-28 14:09:03 -0700

    link to artifact page on /

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,11 @@ import Layout from 'components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Here's the stuff that I've made</h1>
+    <h2>
+    <Link to="/artifacts">Artifacts</Link>
+    </h2>
+
   </Layout>
 )
 
