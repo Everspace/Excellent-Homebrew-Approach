c621c9f6e3cb01adaf5005b936f6089083cafd0b
Everspace <7560635+Everspace@users.noreply.github.com>
2018-09-03 15:48:40 -0700

    extract CSS out of Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,36 @@
 // Default layout
 // tslint:disable
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
+import { css } from "emotion"
+import { classNames } from "style/common"
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+const titleBarStyle = css`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const headerHolderStyle = css`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const headerStyle = css`
+  margin: 0px;
+`
+
+const headerLinkStyle = css`
+  color: white;
+  text-decoration: none;
+  cursor: pointer;
+`
+
+const Header = ({ siteTitle, ...props }) => (
+  <div className={classNames(props, titleBarStyle)}>
+    <div className={headerHolderStyle}>
+      <h1 className={headerStyle}>
+        <Link to="/" className={headerLinkStyle}>
           {siteTitle}
         </Link>
       </h1>
