5fd564227d49ccdcccf793192fc9dc7be550d28d
Everspace <7560635+Everspace@users.noreply.github.com>
2018-09-03 19:06:42 -0700

    Await evocation node to be made, also linking stuff is dumb, can do a better query instead

--- plugins/gatsby-remark-exalted/src/artifact.js
+++ plugins/gatsby-remark-exalted/src/artifact.js
@@ -13,7 +13,7 @@ export const createArtifactNode = async props => {
   // All artifacts have a rating, if they don't they're probably
   // an evocation.
   if (typeof node.frontmatter.rating !== "number") {
-    return createEvocationNode(props)
+    return await createEvocationNode(props)
   }
 
   let result = baseNode(props, "Artifact")
@@ -91,67 +91,6 @@ export const createArtifactNode = async props => {
   return result
 }
 
-let artifactToIds = {
-  // name: {
-  //   id: ....
-  //   nodes: []
-  // }
-}
-
-const linkNodes = async (newNode, createParentChildLink, get) => {
-  let artifact = newNode.artifact ? newNode.artifact : newNode.name
-
-  // Make the object to hold if missing.
-  if (!artifactToIds[artifact]) {
-    artifactToIds[artifact] = {
-      nodes: [],
-    }
-  }
-
-  let thingus = artifactToIds[artifact]
-
-  let parent = null
-  let child = null
-
-  // if this is an ExaltedArtifact
-  if (artifact === newNode.name) {
-    // check if id' yet
-    if (!thingus.id) {
-      thingus.id = newNode.id
-    }
-    // and this would be the parent
-    parent = newNode
-  } else {
-    // This is an evocation and we should log our id
-    thingus.nodes.push(newNode.id)
-    // and we're the child
-    child = newNode
-
-    // And if the parent node was already created
-    if (thingus.id) {
-      // We should get that
-      parent = await get(thingus.id)
-    } else {
-      // We can't link anything
-      return
-    }
-  }
-
-  if (parent && child) {
-    // Link the two
-    createParentChildLink({ parent, child })
-    // since we already pushed the last id in,
-    // we can pop and do the nice link and not waste time
-    // on getting the node we have already
-    thingus.nodes.pop()
-    // Then go and do the rest
-    thingus.nodes.map(async evocationID => {
-      child = await get(evocationID)
-      createParentChildLink({ parent, child })
-    })
-  }
-}
-
 /**
  * The Gatsby API
  * @param {*} props
@@ -173,7 +112,6 @@ export const onCreateNode = async function(props, pluginOptions) {
     newNode = digest(newNode)
     createNode(newNode)
     createParentChildLink({ parent: node, child: newNode })
-    await linkNodes(newNode, createParentChildLink, getNode)
   }
 
   return createArtifactNode(props).then(makeNode)
