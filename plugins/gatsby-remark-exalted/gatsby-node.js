e66046a6ae122761fd78624e5d75c4984a1217e7
Everspace <7560635+Everspace@users.noreply.github.com>
2018-09-30 18:40:35 -0700

    Make working with gastby api less dumb by compiling to dist.

---
+++ plugins/gatsby-remark-exalted/gatsby-node.js
@@ -0,0 +1,33 @@
+const myModules = ["artifact"]
+
+/**
+ * Get all api from available modules
+ * @param {string} api Api name
+ * @returns {function[]} valid functions
+ */
+const harvestApis = api => {
+  return myModules
+    .map(name => {
+      try {
+        return require(`./dist/${name}.js`)
+      } catch (e) {
+        console.log(
+          `Could not import gatsby-remark-exalted/src/${name}.${api}: see the following error:`,
+        )
+        console.log(e)
+        return {}
+      }
+    })
+    .filter(obj => obj[api])
+    .map(obj => obj[api])
+}
+
+const onCreateNodeApis = harvestApis("onCreateNode")
+exports.onCreateNode = (props, pluginOptions) => {
+  return onCreateNodeApis.map(fn => fn(props, pluginOptions))
+}
+
+const createPagesApis = harvestApis("createPages")
+exports.createPages = props => {
+  return createPagesApis.map(fn => fn(props))
+}
